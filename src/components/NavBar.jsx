ed837a7b0a5e76fe57e1735c7b18aa00e2641a4f
Isai Xuncax <isaixuncax@isais-air.lan>
2023-09-21 16:41:00 -0700

    finish

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,4 @@
-    // import { Link } from "react-router-dom";
-    //  import { useContext } from "react";
-    //  import { CartContext } from "../cartcontext";
 
-    //  function Nav() {
-        //  const { login, logoutFunc } = useContext(CartContext);
-
-        //  return (
-            //  <div className="topnav">
-           
-                //  <Link to='/products'> <a>Home</a></Link>
-                //  {login ?  (<span onClick={logoutFunc}>Logout</span> ):<Link to='/'> <a>Login</a></Link>}
-                //  <Link to='/shoppingcart'> <a>Cart</a></Link>
-
-            //  </div>
-        //  )
-
-    //  }
-
-    //  export default Nav;
 
     import React, { useContext } from "react";
 import { Link } from "react-router-dom";
